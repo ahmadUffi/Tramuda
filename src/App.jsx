9bf9ecd263a5e60a161426f19ca6451c1406a1a2
AhmadUffi <alhamdul46@gmail.com>
2023-11-09 16:16:00 +0700

    Add a Title, Button Component and Hero Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Filter from "./Components/artikel/filter/Filter";
 import Curosel from "./Components/artikel/curosel/Curosel";
 import Navbar from "./Components/Utility/navbar/Navbar";
 import Footer from "./Components/Utility/footer/Footer";
+import Button from "./Components/Utility/button/Button";
 
 function App() {
   return (
--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -1,5 +1,25 @@
+import "./hero.css";
+import video from "../../assets/video/background.mp4";
+import Button from "../Utility/button/Button";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="background">
+        <video src={video} autoPlay muted loop></video>
+      </div>
+      <div className="description">
+        <h2>
+          Mari Eksplorasi Alat Musik <span> Tradisional Indonesia</span>
+        </h2>
+        <p>
+          Tramuda merupakan Platform edukasi yang memperkenalakan <br /> alat
+          musik tradisional yang ada di Indonesia
+        </p>
+        <Button action="Mulai Belajar" />
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
