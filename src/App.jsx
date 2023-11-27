d7f5f94bfa2fdcdeeff6787ccbb7f38b2fd88f0e
AhmadUffi <alhamdul46@gmail.com>
2023-11-27 22:23:36 +0700

    before marge with contact and acount page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Artikel from "./Components/artikel/Artikel";
 import Navbar from "./Components/Utility/navbar/Navbar";
 import Footer from "./Components/Utility/footer/Footer";
+import Rebab from "./assets/Rebab";
 function App() {
   return (
     <div className="app">
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" index element={<Hero />} />
         <Route path="/beranda" element={<Beranda />} />
+        <Route path="/three" element={<Rebab />} />
         <Route path="/beranda/:artikel" element={<Artikel />} />
       </Routes>
       <Footer />
