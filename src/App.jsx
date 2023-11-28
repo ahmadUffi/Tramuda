ac8fbcce68735d297cf6103a7e5cb52378a0395d
Bangjoule <115203185+Bangjoule@users.noreply.github.com>
2023-11-28 12:32:18 +0700

    Updating 3D + Source

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { Route, Routes } from "react-router-dom";
 import Artikel from "./Components/artikel/Artikel";
 import Navbar from "./Components/Utility/navbar/Navbar";
 import Footer from "./Components/Utility/footer/Footer";
-import Rebab from "./assets/Rebab";
 function App() {
   return (
     <div className="app">
@@ -20,7 +19,6 @@ function App() {
         <Route path="/beranda" element={<Beranda />} />
         <Route path="/akun" element={<Akun />} />
         <Route path="/kontak" element={<Kontak />} />
-        <Route path="/three" element={<Rebab />} />
         <Route path="/beranda/:artikel" element={<Artikel />} />
       </Routes>
       <Footer />
---
+++ src/Components/ThreeD/Threed.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function ThreeD({ threeD }) {
+  console.log(threeD);
+  return (
+    <div className="sketchfab-embed-wrapper">
+      <iframe
+        title="Tifa"
+        frameBorder="0"
+        allowFullScreen
+        mozallowfullscreen="true"
+        webkitallowfullscreen="true"
+        allow="autoplay; fullscreen; xr-spatial-tracking"
+        xr-spatial-tracking
+        execution-while-out-of-viewport
+        execution-while-not-rendered
+        web-share
+        width="100%"
+        height="420"
+        src={`https://sketchfab.com/models/${threeD}/embed?autospin=1&autostart=1&ui_theme=dark`}
+      ></iframe>
+    </div>
+  );
+}
+
+export default ThreeD;
