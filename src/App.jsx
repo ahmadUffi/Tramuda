13b14d291ea4f749802d4a3cda8057ccccd69daa
ahmadUffi <144457661+ahmadUffi@users.noreply.github.com>
2023-12-19 13:36:50 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import Akun from "./Components/akun/Akun";
 import Kontak from "./Components/kontak/Kontak";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Artikel from "./Components/artikel/Artikel";
-import Register from "./Components/auth/register/register";
 
 function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/register" element={<Register />} />
         <Route path="/" index element={<Hero />} />
         <Route path="/home" element={<Beranda />} />
         <Route path="/akun" element={<Akun />} />
